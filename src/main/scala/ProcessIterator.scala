781117aa6a8e6c82247c29049402552ed5bcbfd7
stofu1234 <yamaboku@gmail.com>
2016-04-25 19:01:17 +0900

    リファクタ後

--- src/main/scala/ProcessIterator.scala
+++ src/main/scala/ProcessIterator.scala
@@ -9,25 +9,23 @@ import java.util.concurrent.BlockingQueue
 import java.util.concurrent.LinkedBlockingQueue
 import java.io.BufferedReader
 import java.io.InputStreamReader
-import java.io.PrintWriter
-import java.util.Date
+
 
 class ProcessIterator(queueSize: Int, args: String*) extends Iterator[String] {
-   val pb      = new ProcessBuilder(args.toList)
-   val process = pb.start()
-   val br      = new BufferedReader(new InputStreamReader(process.getInputStream()))
-   val er      = new BufferedReader(new InputStreamReader(process.getErrorStream()))
-   val exec    = Executors.newFixedThreadPool(3)
-   val queue: BlockingQueue[Any] = new LinkedBlockingQueue[Any](queueSize)
+
+   val elementQueue: BlockingQueue[Any]     = new LinkedBlockingQueue[Any](queueSize)
    val hasNextQueue: BlockingQueue[Boolean] = new LinkedBlockingQueue[Boolean](queueSize)
-   var isFirstRead = false
-   var isOutputEnd = false
-   var isErrorEnd  = false
-   var isAllEnd    = false
-   val pw = new PrintWriter(".\\out\\result_ProcessIterator_"+args(0)+".log")
+   val pb               = new ProcessBuilder(args.toList)
+   val process          = pb.start()
+   val br               = new BufferedReader(new InputStreamReader(process.getInputStream()))
+   val er               = new BufferedReader(new InputStreamReader(process.getErrorStream()))
+   val exec             = Executors.newFixedThreadPool(3)
+   var isFirstRead      = false
+   var isOutputEnd      = false
+   var isErrorEnd       = false
+   val initialReadWait  = 100
 
    case class MessageElement(_message: String)
-   case class EOFElement()
    case class ExceptionElement(ex: Exception)
 
    implicit def funcToRunnable( func : () => Unit ) = new Runnable(){ def run() = func() }
@@ -37,114 +35,52 @@ class ProcessIterator(queueSize: Int, args: String*) extends Iterator[String] {
             var nextLine=reader.readLine()
             isFirstRead = true
             while(nextLine!=null){
-               pw.println(("%tF %<tT.%<tL" format new Date)+" nextLine:"+nextLine)
-               pw.flush()
-               queue.put(new MessageElement(nextLine))
+               elementQueue.put(new MessageElement(nextLine))
                hasNextQueue.put(true)
                nextLine=reader.readLine()
             }
-            //var nextLine=""
-            //this.synchronized {
-            //   nextLine=reader.readLine()
-            //   isFirstRead = true
-            //   queue.put(new MessageElement(nextLine))
-            //}
-            //while(nextLine!=null){
-            //   this.synchronized {
-            //      nextLine=reader.readLine()
-            //      queue.put(new MessageElement(nextLine))
-            //   }
-            //}
             reader.close()
-            pw.println(("%tF %<tT.%<tL" format new Date)+" after asyncBuffering")
-            pw.flush()
          } catch {
             case e: Exception => {
-                                  queue.put(new ExceptionElement(e))
+                                  elementQueue.put(new ExceptionElement(e))
                                   hasNextQueue.put(true)
                                  }
          }
       }
    
+   //標準入力の非同期読み込み
    exec.execute(() => {
                          asyncBuffering(br)
                          isOutputEnd = true
-                         if(isOutputEnd && isErrorEnd && queue.size == 0) {
-                            pw.println(("%tF %<tT.%<tL" format new Date)+" before put EOFElement")
-                            pw.flush()
-                            isAllEnd = true
+                         if(isOutputEnd && isErrorEnd && elementQueue.size == 0) {
                             hasNextQueue.put(false)
-                            //queue.put(new EOFElement)
                          }
                       })
+   //標準エラーの非同期読み込み
    exec.execute(() => {
                          asyncBuffering(er)
                          isErrorEnd  = true
-                         if(isOutputEnd && isErrorEnd && queue.size == 0) {
-                            pw.println(("%tF %<tT.%<tL" format new Date)+" before put EOFElement")
-                            pw.flush()
-                            isAllEnd = true
+                         if(isOutputEnd && isErrorEnd && elementQueue.size == 0) {
                             hasNextQueue.put(false)
-                            //queue.put(new EOFElement)
                          }
                       })
 
    def this(args: String*) = this(1024,args:_*)
 
    def hasNext(): Boolean = {
-      //最初の行でExceptionが発生した場合を考慮し、queue.size>0追加
-      while(!(isFirstRead || queue.size > 0)){
-         Thread.sleep(100)
+      //最初の行でExceptionが発生した場合を考慮し、elementQueue.size>0追加
+      while(!(isFirstRead || elementQueue.size > 0)){
+         Thread.sleep(initialReadWait)
       }
-      //return queue.size>0
-      //return !(isOutputEnd && isErrorEnd)
-      //return (!(isOutputEnd && isErrorEnd) || queue.size > 0) && !isAllEnd
-      pw.println(("%tF %<tT.%<tL" format new Date)+" hasNext:"+((!(isOutputEnd && isErrorEnd) || queue.size > 0) && !isAllEnd))
-      pw.println(("%tF %<tT.%<tL" format new Date)+" isOutputEnd:"+isOutputEnd+" isErrorEnd:"+isErrorEnd+" queue.size:"+queue.size+" isAllEnd:"+isAllEnd)
-      pw.flush()
-      this.synchronized {
-         //return (!(isOutputEnd && isErrorEnd) || queue.size > 0) && !isAllEnd
-         return !(isOutputEnd && isErrorEnd) && hasNextQueue.take() || queue.size > 0
-      }
+      return !(isOutputEnd && isErrorEnd) && hasNextQueue.take() || elementQueue.size > 0
    }
    def next(): String = {
-      pw.println(("%tF %<tT.%<tL" format new Date)+" enter next")
-      pw.flush()
-      //if(queue.size>0){
-      //if(!(isOutputEnd && isErrorEnd)){
-      if(!(isOutputEnd && isErrorEnd) || queue.size > 0){
-         pw.println(("%tF %<tT.%<tL" format new Date)+" before next take")
-         pw.flush()
-         val element = queue.take()
-         pw.println(("%tF %<tT.%<tL" format new Date)+" after  next take")
-         pw.flush()
-         
-         element match {
-            case MessageElement(message) => {
-                                             pw.println(("%tF %<tT.%<tL" format new Date)+" before MessageElement:"+message)
-                                             pw.println(("%tF %<tT.%<tL" format new Date)+" isOutputEnd:"+isOutputEnd+" isErrorEnd:"+isErrorEnd+" queue.size:"+queue.size+" isAllEnd:"+isAllEnd)
-                                             pw.flush()
-                                             return message
-                                            }
-            case EOFElement() => {
-                                  pw.println(("%tF %<tT.%<tL" format new Date)+" before EOFElement")
-                                  pw.flush()
-                                  return null
-                                 }
-            //case EOFElement() => throw new NoSuchElementException()
-            case ExceptionElement(ex) => throw ex
-            case _ => {
-                       pw.println(("%tF %<tT.%<tL" format new Date)+" unknown type:"+element.getClass().getName())
-                       pw.flush()
-                       return null
-                      }
-            //case _ => throw new NoSuchElementException()
-         }
+      val element = elementQueue.take()
+      element match {
+         case MessageElement(message) => return message
+         case ExceptionElement(ex)    => throw ex
+         case _                       => throw new NoSuchElementException()
       }
-      pw.println(("%tF %<tT.%<tL" format new Date)+" end of next")
-      pw.flush()
-      return null
-      //throw new NoSuchElementException()
    }
 }
 
--- src/main/scala/StreamElement.scala
+++ src/main/scala/StreamElement.scala
@@ -4,7 +4,6 @@ trait StreamElement
 class MessageElement(_message: String) extends StreamElement {
    var message = _message
 }
-class EOFElement extends StreamElement
 class ExceptionElement(_ex: Exception) extends StreamElement {
    var ex = _ex
 }
